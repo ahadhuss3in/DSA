79f1eb819b99750056d7c2df155607cd4f51b3b2
ahadhuss3in <ahadasif.cs22@sahyadri.edu.in>
2025-04-14 13:27:49 +0530

    floor of a number

--- floor.java
+++ floor.java
@@ -1,14 +1,13 @@
-// to find the smallest number greater than the target number in a array;
-
-public class BinarysearchCieling {
+//find the largest number smaller than the target
+public class floor {
     public static void main(String[] args) {
         int[] arr = {1,2,4,5,7,19,22,23,28};
         int target = 20;
-        int ans = cieling(arr,target);
+        int ans = floor(arr,target);
         System.out.println(arr[ans]);
     }
 
-    public static int cieling(int[] arr , int target){
+    public static int floor(int[] arr , int target){
         int start = 0 ;
         int end = arr.length-1;
         if(target > arr[arr.length-1]){
@@ -24,13 +23,12 @@ public class BinarysearchCieling {
                 start = mid+1;
             }
             else{
-                return mid+1;
+                return mid-1;
             }
 
         }
 
-        return  start ;//return the start element , since the loop exists when condition start>end comes true
-        // that means its in the next element after target
+        return  end ;//return the end element , since the loop exists when condition start>end comes true
+        // that means its in the prev element before target
     }
-
 }
